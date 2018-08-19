886a399fa31862807df59e0372c38d342122575c
Marc Leidner <marc@viadesk.nl>
2018-08-19 13:18:52 +0600

    removed sysouts

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -7,13 +7,7 @@ import com.module.app.Main;
 public class App {
 	public boolean someAppMethod() {
 
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("5");
-		System.out.println("6");
-		System.out.println("7");
+		System.out.println("bug fixed");
 		
 		return new Main().test();
 		// return false;
