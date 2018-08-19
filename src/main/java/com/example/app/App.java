b21ce43defda55d16f3ef93e69356337bea298f7
Rubel Ahammad <rubel.ahammad@gmail.com>
2018-08-19 12:57:51 +0600

    sysout7r

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -11,10 +11,9 @@ public class App {
 		System.out.println("2");
 		System.out.println("3");
 		System.out.println("4");
-		System.out.println("6");
-		
 		System.out.println("5");
-		
+		System.out.println("6");
+		System.out.println("7");
 		
 		return new Main().test();
 		// return false;
